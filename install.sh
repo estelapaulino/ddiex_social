b0e891c71173a69e9afa8d9eed0c856b0bfeb255
Estela Paulino <estela.paulino@gmail.com>
2012-03-09 18:32:05 -0300

    inserção do readme e script de instalação

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+# Leia o arquivo readme antes de executar a instalação 
+# Começando a instalação do DDIEX Social
+
+
+sudo apt-get install postgresql-8.3 postgresql-client-8.3
+
+sudo apt-get install pgadmin3 pgadmin3-data
+
+sudo apt-get install python-setuptools
+
+sudo easy_install django
+
+sudo easy_install python-psycopg2
+
+sudo easy_install PIL
+
+sudo easy_install django-piston
+
+
+
